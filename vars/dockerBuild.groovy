86b36417b23e488719705221c1597cdfdd80fb74
rajesh <rajeshgovindan777@gmail.com>
2026-08-31 16:25:44 +0530

    added trivy shared lib

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,32 +1,50 @@
-def call(Map config){
-  //required 
-  def service = config.service 
-  def contextPath = config.contextPath ?: "services/${service}"
-
-  //optional 
-
-  def registry = config.registry ?: 'rajesh00007'
-  def platform = config.platform ?: 'linux/amd64'
-  def tag = config.tag ?: env.BUILD_NUMBER
+def call(Map config) {
+    def service = config.service
+    def contextPath = config.contextPath ?: "services/${service}"
+    def registry = config.registry ?: 'rajesh00007'
+    def platform = config.platform ?: 'linux/amd64'
+    def push = config.containsKey('push') ? config.push : true
+    def tag = config.tag ?: env.BUILD_NUMBER
     def builderName = "${service}-builder"
     def imageRef = "${registry}/${service}:${tag}"
     def cacheRef = "${registry}/${service}:buildcache"
 
-  sh """
-  docker buildx create \\
-          --name ${builderName} \\
-          --driver docker-container \\
-          --use || docker buildx use ${builderName}
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerhub-creds',
+        usernameVariable: 'USER',
+        passwordVariable: 'PASS')]) {
 
-        docker buildx inspect --bootstrap
+        withEnv([
+            "SERVICE=${service}",
+            "IMAGE_REF=${imageRef}",
+            "CACHE_REF=${cacheRef}",
+            "BUILDER=${builderName}",
+            "PLATFORM=${platform}",
+            "CONTEXT=${contextPath}"
+        ]) {
+            try {
+                sh '''
+                    echo ${PASS} | docker login -u "${USER}" --password-stdin
 
-     docker buildx build \\
-          --builder ${builderName} \\
-          --platform ${platform} \\
-          --tag ${imageRef} \\
-          --cache-from type=registry,ref=${cacheRef} \\
-          --cache-to type=registry,ref=${cacheRef},mode=max \\
-          --push \\
-          ${contextPath}   
-  """
+                    docker buildx create \
+                      --name ${BUILDER} \
+                      --driver docker-container \
+                      --use || docker buildx use ${BUILDER}
+
+                    docker buildx inspect --bootstrap
+
+                    docker buildx build \
+                      --builder ${BUILDER} \
+                      --platform ${PLATFORM} \
+                      --tag ${IMAGE_REF} \
+                      --cache-from type=registry,ref=${CACHE_REF} \
+                      --cache-to type=registry,ref=${CACHE_REF},mode=max \
+                      --push \
+                      ${CONTEXT}
+                '''
+            } finally {
+                sh 'docker logout'
+            }
+        }
+    }
 }
