da8ac374eb4652d85c34f2af53cdb3f83d917038
rajesh <rajeshgovindan777@gmail.com>
2026-08-31 16:13:18 +0530

    added trivy shared lib

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,20 +1,35 @@
-def call (Map config) {
-  def service = config.service 
-  def registry = 'rajesh00007'
-  def tag = env.BUILD_NUMBER 
+def call(Map config) {
+    def service = config.service
+    def registry = 'rajesh00007'
+    def tag = env.BUILD_NUMBER
+    def cacheDir = "/tmp/trivy-cache-${service}-${env.BUILD_NUMBER}"
 
-  withCredentials([usernamePassword(
-    credentialsId: 'dockerhub-creds', 
-    usernameVariable: 'USER', 
-    passwordVariable: 'PASS')]) {
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerhub-creds',
+        usernameVariable: 'USER',
+        passwordVariable: 'PASS')]) {
 
-          sh """
-           echo ${PASS} | docker login -u "${USER}" --password-stdin 
+        withEnv(["SERVICE=${service}", "REGISTRY=${registry}", "TAG=${tag}", "CACHE_DIR=${cacheDir}"]) {
+            try {
+                sh '''
+                    echo ${PASS} | docker login -u "${USER}" --password-stdin
 
-            trivy image --exit-code 1 \
-             --severity CRITICAL \
-             --ignore-unfixed \
-              ${registry}/${service}:${tag}
-          """
-          }
+                    mkdir -p ${CACHE_DIR}
+                    cp -r /tmp/trivy-shared-db/db ${CACHE_DIR}/db
+
+                    trivy image --exit-code 1 \
+                      --severity CRITICAL \
+                      --ignore-unfixed \
+                      --skip-db-update \
+                      --cache-dir ${CACHE_DIR} \
+                      ${REGISTRY}/${SERVICE}:${TAG}
+                '''
+            } finally {
+                sh '''
+                    docker logout
+                    rm -rf ${CACHE_DIR}
+                '''
+            }
+        }
+    }
 }
