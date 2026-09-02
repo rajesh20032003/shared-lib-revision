dcbaffc648733d544ff151b3b9281f87301ac3c2
rajesh <rajeshgovindan777@gmail.com>
2026-09-02 11:23:19 +0530

    added dtrack shared-lib

--- vars/sec.groovy
+++ vars/sec.groovy
@@ -1,7 +1,32 @@
 def call(Map config) {
-  sh '''
-  gitleaks detect --source=. --report=gitleaks-report.json --report-format=json
+    def service = config.service
+    def outputDir = "artifacts/${service}"
+    def gitleaksReport = "${outputDir}/gitleaks-report.json"
+    def trivyFsReport = "${outputDir}/trivy-fs-report.json"
 
-  trivy fs --scanners vuln,secret,misconfig .
-  '''
+    withEnv([
+        "SERVICE_PATH=services/${service}",
+        "OUT_DIR=${outputDir}",
+        "GITLEAKS_OUT=${gitleaksReport}",
+        "TRIVY_FS_OUT=${trivyFsReport}"
+    ]) {
+        sh '''
+            mkdir -p ${OUT_DIR}
+
+            gitleaks detect \
+              --source=${SERVICE_PATH} \
+              --report-path=${GITLEAKS_OUT} \
+              --report-format=json \
+              --no-git \
+              --exit-code=1
+
+            trivy fs \
+              --scanners vuln,secret,misconfig \
+              --format json \
+              --output ${TRIVY_FS_OUT} \
+              ${SERVICE_PATH}
+        '''
+    }
+
+    archiveArtifacts artifacts: "${gitleaksReport},${trivyFsReport}", allowEmptyArchive: true
 }
