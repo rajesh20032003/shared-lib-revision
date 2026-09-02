124ce77baaf8c507fc79699ab19dc65c5a54277c
rajesh <rajeshgovindan777@gmail.com>
2026-09-02 10:47:23 +0530

    added dtrack shared-lib

--- vars/sbomGen.groovy
+++ vars/sbomGen.groovy
@@ -1,21 +1,26 @@
 def call(Map config) {
-  def service = config.service 
-  def registry = 'rajesh00007'
-  def tag = env.BUILD_NUMBER
-  def outputFile = "${service}-${tag}.json"
-  withCredentials([usernamePassword(
+    def service = config.service
+    def registry = 'rajesh00007'
+    def tag = env.BUILD_NUMBER
+    def outputDir = "artifacts/${service}"
+    def outputFile = "${outputDir}/sbom.json"
+
+    withCredentials([usernamePassword(
         credentialsId: 'dockerhub-creds',
         usernameVariable: 'DOCKER_USER',
         passwordVariable: 'DOCKER_PASS')]) {
 
-      withEnv([
-        "SERVICE=${service}", 
-        "REGISTRY=${registry}", 
-        "TAG=${tag}", 
-        "OUT=${outputFile}"]) {
+        withEnv([
+            "SERVICE=${service}",
+            "REGISTRY=${registry}",
+            "TAG=${tag}",
+            "OUT_DIR=${outputDir}",
+            "OUT=${outputFile}"
+        ]) {
             try {
                 sh '''
                     printf '%s' "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+                    mkdir -p ${OUT_DIR}
                     syft ${REGISTRY}/${SERVICE}:${TAG} -o cyclonedx-json=${OUT}
                 '''
             } finally {
@@ -25,5 +30,4 @@ def call(Map config) {
     }
 
     archiveArtifacts artifacts: outputFile, fingerprint: true
-
-        }
+}
