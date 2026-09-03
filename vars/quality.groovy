5ec4aac3d1c77791fdc6935eb91eaf40dc8365d3
rajesh <rajeshgovindan777@gmail.com>
2026-09-03 13:11:13 +0530

    finxed sonarqube

--- vars/quality.groovy
+++ vars/quality.groovy
@@ -1,9 +1,10 @@
-// vars/sonarQualityGate.groovy
 def call() {
     timeout(time: 5, unit: 'MINUTES') {
         def qg = waitForQualityGate()
         if (qg.status != 'OK') {
             error "SonarQube Quality Gate failed: ${qg.status}"
+        } else {
+            echo "SonarQube Quality Gate passed"
         }
     }
 }
--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -2,13 +2,15 @@ def call(Map config) {
     def service = config.service
     def sourcePath = "services/${service}"
     def projectKey = "monorepo-${service}"
-    def scannerHome = tool 'SonarScanner'   // must match the Name field exactly: "SonarScanner"
+    def workDir = ".scannerwork-${service}"
+    def scannerHome = tool 'SonarScanner'
 
     withSonarQubeEnv('SonarQube') {
         withEnv([
             "SERVICE=${service}",
             "SOURCE_PATH=${sourcePath}",
             "PROJECT_KEY=${projectKey}",
+            "WORK_DIR=${workDir}",
             "PATH+SONAR=${scannerHome}/bin"
         ]) {
             sh '''
@@ -16,6 +18,7 @@ def call(Map config) {
                   -Dsonar.projectKey=${PROJECT_KEY} \
                   -Dsonar.sources=${SOURCE_PATH} \
                   -Dsonar.projectName=${SERVICE} \
+                  -Dsonar.working.directory=${WORK_DIR} \
                   -Dsonar.exclusions=**/node_modules/**,**/vendor/**,**/test/**,**/tests/**,**/.git/**,**/*.spec.js,**/*.test.js,**/*.spec.rb,**/*.test.rb
             '''
         }
